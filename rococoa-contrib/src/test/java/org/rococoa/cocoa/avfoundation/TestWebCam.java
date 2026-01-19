cd4512298235417abbb6c330d782431b235d4560
Naohide Sano <namachapanda2005@gmail.com>
2026-01-19 21:21:41 +0900

    [logging] let settings into logging.properties

--- rococoa-contrib/src/test/java/org/rococoa/cocoa/avfoundation/TestWebCam.java
+++ rococoa-contrib/src/test/java/org/rococoa/cocoa/avfoundation/TestWebCam.java
@@ -57,20 +57,6 @@ import static org.rococoa.cocoa.vision.VNHumanHandPoseObservation.ThumbTip;
 public class TestWebCam {
 
     static {
-        // TODO move to pom.xml
-        System.setProperty("vavi.util.logger.VaviFormatter.extraClassMethod",
-                "(" +
-                        "org\\.slf4j\\.impl\\.JDK14LoggerAdapter#(log|info|warn)" +
-                        "|" +
-                        "sun\\.util\\.logger\\.LoggingSupport#log" +
-                        "|" +
-                        "sun\\.util\\.logger\\.PlatformLogger#fine" +
-                        "|" +
-                        "jdk\\.internal\\.event\\.EventHelper#logX509CertificateEvent" +
-                        "|" +
-                        "sun\\.util\\.logger\\.PlatformLogger.JavaLoggerProxy#doLog" +
-                        ")");
-
         Webcam.setDriver(new NativeDriver());
     }
 
